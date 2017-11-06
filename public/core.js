d20fc4e73f80f8f6a08e64704a0ecacb00ee6052
Karri Lehtiranta <lehtirk1@aalto.fi>
2017-11-06 21:30:06 +0200

    Web page working now, simple mockup UI with no funtionality

--- public/core.js
+++ public/core.js
@@ -1,11 +1,10 @@
-var cryptoExchange = angular.module('cryptoExchange', []);
-
-function mainController($scope, $http) {
-    $scope.formData = {};
-    stocks = [{"name":"Test", "price":"9.342", "amount":"15"},
+var cryptoExchange = angular.module('cryptoExchange', [])
+  .controller('mainController', function() {
+    var stockPage = this;
+    var stocks = [{"name":"Test", "price":"9.342", "amount":"15"},
               {"name":"Test", "price":"9.342", "amount":"15"},
               {"name":"Test", "price":"9.342", "amount":"15"},
               {"name":"Test", "price":"9.342", "amount":"15"}];
 
-    $scope.stocks = stocks;
-}
+    stockPage.stocks = stocks;
+  });
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('*', function(req, res) {
-        res.sendfile('public/index.html');
-    });
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res) {
+    res.sendFile('public/index.html', { root : __dirname});
+});
+
 
 app.listen(5000, function() {
     console.log("Listening on port 5000");
