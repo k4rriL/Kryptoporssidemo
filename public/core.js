cc55109d2e3cb1b8a73bb58e2f39af082bfeae60
Karri Lehtiranta <karri.lehtiranta@aalto.fi>
2017-11-07 10:41:04 +0200

    ADd more functionality to UI, start making new pages

--- public/core.js
+++ public/core.js
@@ -1,10 +1,18 @@
 var cryptoExchange = angular.module('cryptoExchange', [])
   .controller('mainController', function() {
     var stockPage = this;
-    var stocks = [{"name":"Test", "price":"9.342", "amount":"15"},
-              {"name":"Test", "price":"9.342", "amount":"15"},
-              {"name":"Test", "price":"9.342", "amount":"15"},
-              {"name":"Test", "price":"9.342", "amount":"15"}];
+    var stocks = [{"name":"Test", "price":"9.342", "amount":"15", "id": 0},
+              {"name":"Test", "price":"9.342", "amount":"15", "id": 1},
+              {"name":"Test", "price":"9.342", "amount":"15", "id": 2},
+              {"name":"Test", "price":"9.342", "amount":"15", "id": 3}];
 
     stockPage.stocks = stocks;
+    stockPage.buying = false;
+    stockPage.currentStock = {};
+    stockPage.buyStock = function (event) {
+      console.log("adsf");
+      event.preventDefault();
+      stockPage.buying = true;
+      stockPage.currentStock = stocks[parseInt($(event.target).attr('href'))];
+    }
   });
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
     res.sendFile('public/index.html', { root : __dirname});
 });
 
+app.get('/my_stocks', function(req, res) {
+    res.sendFile('public/my_stocks.html', { root : __dirname});
+});
+
 
 app.listen(5000, function() {
     console.log("Listening on port 5000");
