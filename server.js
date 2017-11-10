0c149c30400a4b75cc467b4c06cd5ba905e7f3cf
Karri Lehtiranta <karri.lehtiranta@aalto.fi>
2017-11-10 11:30:37 +0200

    Add more stuff to UI

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.get('/my_stocks', function(req, res) {
     res.sendFile('public/my_stocks.html', { root : __dirname});
 });
 
-
 app.listen(5000, function() {
     console.log("Listening on port 5000");
 });
