3d57cf3e25643f3aec2cc0e168f71197297c2343
Ekholm Mikael <ekholmm1@niksula.hut.fi>
2017-11-20 13:15:09 +0200

    Changed port back to original

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bc = require('./blockchain.js');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
